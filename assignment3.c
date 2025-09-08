047cac42bb6581a5f91e310b7f9052e73c90796a
shrishantjadhav <shrishantjadhav02@gmail.com>
2025-09-08 09:20:14 +0530

    assignment3 updated

--- assignment3.c
+++ assignment3.c
@@ -1,103 +1,112 @@
 #include <stdio.h>
-#include <stdlib.h> // for malloc(), free(), exit()
+#include <stdlib.h>
+#include <string.h>
 
-int main(void)
-{
-    //varaible declarations
-    int** magic_square = NULL;
-    int n,num;
-    int row, col, next_row, next_col;
-    int i, j;
+#define NAME_LEN 32
+#define DIV_LEN 10
+#define BRANCH_LEN 32
+#define CITY_LEN 15
+#define CLASS_LEN 32
 
-    //Code
 
-    //1: Read n. If n <= 0 or n is even → stop with error (works only for odd n).
-    printf("Enter the value of n : ");
+typedef struct Student {
+    int roll;
+    char name[NAME_LEN];
+    char div[DIV_LEN];
+    char branch[BRANCH_LEN];
+    char classs[CLASS_LEN];
+    char city[CITY_LEN];
+}Student;
+
+int main(void) {
+
+    int n, i;
+    int choice;
+    printf("Enter the number of records you want: ");
     scanf("%d", &n);
 
-    if (n <= 0 || n % 2 == 0)
-    {
-        printf("Error: n must be positive Odd Number.");
+    Student* st = NULL;
+    st = (Student*)malloc(sizeof(Student) * n);
+    if (st == NULL) {
+        printf("Memory allocation failed!");
         exit(-1);
     }
 
-    //2. Dynamically allocate an n × n matrix and initialize every cell to 0.
-    magic_square =(int**)malloc(sizeof(int*) * n);
+    const char* names[] = { "Abir","Aarav","Isha","Rohan","Priya","Vikas","Neha","Sahil","Anaya","Dev","Kriti",
+        "Mira","Kabir","Tanvi","Yash","Riya","Arjun","Asha","Nikhil","Pooja","Kunal", "Vishal","Amir","Sharukh","Salman","Mrunal","Kirti" };
+    const int name_size = (int)(sizeof(names) / sizeof(names[0]));
 
-    if (magic_square == NULL)
-    {
-        printf("Memory allocation failed. \n");
-        exit(-2);
+    const char* cities[] = { "Amravati","Akola","Nagar","Pune","Mumbai","Nashik","Nagpur","Aurangabad","Thane","Satara","Solapur","Kolhapur","Nanded" };
+    const int city_size = (int)(sizeof(cities) / sizeof(cities[0]));
+
+    const char* branches[] = { "COMP","IT","AIDS","ENTC","MECH","CIVIL", "AIML", "IOT" };
+    const int branch_size = (int)(sizeof(branches) / sizeof(branches[0]));
+
+    const char* division[] = { "A","B","C","D" };
+    const int div_size = (int)(sizeof(division) / sizeof(division[0]));
+
+    const char* classes[] = { "FE","SE","TE","BE" };
+    const int class_size = (int)(sizeof(classes) / sizeof(classes[0]));
+
+    for (i = 0; i < n; i++) {
+        strcpy(st[i].name, names[rand() % name_size]);
+        strcpy(st[i].classs, classes[rand() % class_size]);
+        strcpy(st[i].div, division[rand() % div_size]);
+        strcpy(st[i].city, cities[rand() % city_size]);
+        strcpy(st[i].branch, branches[rand() % branch_size]);
+        st[i].roll = i + 1;
+    }
+    printf("\n\n---------------------All records---------------------\n\n");
+    for (i = 0; i < n; i++) {
+        printf("\n\nRoll No: %d | Name: %s | Class: %s | Branch: %s | Division: %s | City: %s", st[i].roll, st[i].name, st[i].classs, st[i].branch, st[i].div, st[i].city);
     }
 
-    for (i = 0; i < n; i++)
-    {
-        magic_square[i] = (int*)malloc(sizeof(int) * n);
-        if (magic_square[i] == NULL)
-        {
-            printf("Memory allcoation failed. \n");
-            exit(-3);
+    while (choice != 0) {
+        printf("\n\n1)Search by name: \n2)Search by roll no. \n3)Print all records \n0)Exit");
+        printf("\nEnter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 0:
+            exit(0);
+            break;
+
+            case 1:
+            char targetName[NAME_LEN];
+            printf("Enter the name you want to search (*case sensitive*): ");
+            scanf("%s", &targetName);
+            for (i = 0; i < n; i++) {
+                if (strcmp(st[i].name, targetName) == 0) {
+                    printf("\n\nRoll No: %d | Name: %s | Class: %s | Branch: %s | Division: %s | City: %s", st[i].roll, st[i].name, st[i].classs, st[i].branch, st[i].div, st[i].city);
+                }
+
+            }
+            break;
+
+            case 2:
+            int targetNo;
+            printf("Enter the number you want to search: ");
+            scanf("%d", &targetNo);
+            for (i=0; i<n; i++){
+                if (st[i].roll == targetNo){
+                    printf("\n\nRoll No: %d | Name: %s | Class: %s | Branch: %s | Division: %s | City: %s", st[i].roll, st[i].name, st[i].classs, st[i].branch, st[i].div, st[i].city);
+                }
+            }
+            break;
+
+            case 3:
+            for(i=0; i<n; i++){
+                 printf("\n\nRoll No: %d | Name: %s | Class: %s | Branch: %s | Division: %s | City: %s", st[i].roll, st[i].name, st[i].classs, st[i].branch, st[i].div, st[i].city);
+            }
+            break;
+
+            default:
+            printf("Enter correct choice.");
+            break;
+
         }
     }
-    //initlialize all the cells to 0
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            magic_square[i][j] = 0;
-        }
-    }
+    free(st);
+    return 0;
 
-    //3. Set row = 0 and col = n / 2 (top row, middle column).
-    row = 0;
-    col = n / 2;
-
-    //4. For num from 1 to n*n:
-    for (num = 1; num <= n * n; num++)
-    {
-        //4a. Place num at matrix[row][col].
-        magic_square[row][col] = num;
-
-        // 4b. Compute tentative next position: next_row = row - 1 (move up), next_col = col + 1 (move right).
-        next_row = row - 1;
-        next_col = col + 1;
-
-        //4c. Wrap around the edges: if next_row < 0 → next_row = n - 1; if next_col == n → next_col = 0.
-        if (next_row < 0)
-            next_row = n - 1;
-        if (next_col == n)
-            next_col = 0;
-
-        //4d. If matrix[next_row][next_col] != 0 (occupied), then instead move down one from the current cell:
-            //next_row = (row + 1) % n;
-            //next_col = col.
-        if (magic_square[next_row][next_col] != 0)
-        {
-            next_row = (row + 1) % n;
-            next_col = col;
-        }
-
-        //4e. Update row = next_row; col = next_col.
-        row = next_row;
-        col = next_col;
-     }
-
-    //5. Print the matrix and the magic sum.
-     for (int i = 0; i < n; i++) 
-     {
-       for (int j = 0; j < n; j++) 
-       {
-            printf("%4d", magic_square[i][j]);
-       }
-        printf("\n");
-     }
-
-     printf("Magic Sum : %d\n\n", n*(n * n + 1) / 2);
-
-    //6. Free all dynamically allocated memory.
-     for (i = 0; i < n; i++)
-         free(magic_square[i]);
-     free(magic_square);
-
-     return(0);
 }
